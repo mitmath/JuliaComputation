dd13e34c83fd8182302fdb7944ea521e5de095a7
Alan Edelman <edelman.alan@gmail.com>
2024-09-25 08:41:06 -0400

    Update 6_MultDisAmbiguity.jl

--- notebooks/6_MultDisAmbiguity.jl
+++ notebooks/6_MultDisAmbiguity.jl
@@ -27,14 +27,17 @@ f(a::Integer, b:: Number) = "(ℤ , ℝ)"
 # ╔═╡ e5d21d57-856e-4c15-87fc-82471afceb35
 f(a::Number, b:: Number) = "(ℝ , ℝ)"
 
+# ╔═╡ 7d98b956-7445-4ec1-8fff-db27ff7ab3dd
+f(1.0,1.0)
+
 # ╔═╡ a9fcb40a-549d-445d-892e-3e0c8e33237f
 f(1.0,1)
 
 # ╔═╡ f45c6ce8-6f93-4c5f-8f7e-198a5203a039
 f(1,1.0)
 
-# ╔═╡ 7d98b956-7445-4ec1-8fff-db27ff7ab3dd
-f(1.0,1.0)
+# ╔═╡ 4a8c885f-2c02-4cb6-968b-96a0d0f6ec9d
+md"Ambiguous:"
 
 # ╔═╡ acdc9c3f-d302-4bc4-b920-f227f3004f38
 f(1,1)
@@ -322,9 +325,10 @@ version = "17.4.0+2"
 # ╠═f01c27c9-e246-4a11-a856-5e1103b541db
 # ╠═83a1215f-93fe-4844-8cd8-2243cd4f2925
 # ╠═e5d21d57-856e-4c15-87fc-82471afceb35
+# ╠═7d98b956-7445-4ec1-8fff-db27ff7ab3dd
 # ╠═a9fcb40a-549d-445d-892e-3e0c8e33237f
 # ╠═f45c6ce8-6f93-4c5f-8f7e-198a5203a039
-# ╠═7d98b956-7445-4ec1-8fff-db27ff7ab3dd
+# ╟─4a8c885f-2c02-4cb6-968b-96a0d0f6ec9d
 # ╠═acdc9c3f-d302-4bc4-b920-f227f3004f38
 # ╟─b861f824-ae14-4a19-bdfa-cde601b1b21c
 # ╟─00000000-0000-0000-0000-000000000001
